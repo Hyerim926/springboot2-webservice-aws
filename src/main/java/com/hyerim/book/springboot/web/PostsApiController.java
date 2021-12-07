a6667eb3b108472fc308670d4b8cebf4ecb3eccc
Hyerim926 <ekfrhsk96@icloud.com>
2021-12-07 22:35:21 +0900

    Spring Security & Social Login

--- src/main/java/com/hyerim/book/springboot/web/PostsApiController.java
+++ src/main/java/com/hyerim/book/springboot/web/PostsApiController.java
@@ -5,6 +5,7 @@ import com.hyerim.book.springboot.web.dto.PostsResponseDto;
 import com.hyerim.book.springboot.web.dto.PostsSaveRequestDto;
 import com.hyerim.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
@@ -13,11 +14,13 @@ public class PostsApiController {
 
     private final PostsService postsService;
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
@@ -28,6 +31,7 @@ public class PostsApiController {
         return postsService.findById(id);
     }
 
+    @PreAuthorize("hasRole('ROLE_USER')")
     @DeleteMapping("/api/v1/posts/{id}")
     public Long delete(@PathVariable Long id) {
         postsService.delete(id);
