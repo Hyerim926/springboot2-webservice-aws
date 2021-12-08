462d2c588580cb0d3d5cfff354ca74fd74fe98be
Hyerim926 <ekfrhsk96@icloud.com>
2021-12-08 17:20:10 +0900

    자유게시판 & 모임 게시판 분리

--- src/main/java/com/hyerim/book/springboot/domain/posts/PostsRepository.java
+++ src/main/java/com/hyerim/book/springboot/domain/posts/PostsRepository.java
@@ -3,7 +3,6 @@ package com.hyerim.book.springboot.domain.posts;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
--- src/main/java/com/hyerim/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/hyerim/book/springboot/service/posts/PostsService.java
@@ -2,10 +2,10 @@ package com.hyerim.book.springboot.service.posts;
 
 import com.hyerim.book.springboot.domain.posts.Posts;
 import com.hyerim.book.springboot.domain.posts.PostsRepository;
-import com.hyerim.book.springboot.web.dto.PostsListResponseDto;
-import com.hyerim.book.springboot.web.dto.PostsResponseDto;
-import com.hyerim.book.springboot.web.dto.PostsSaveRequestDto;
-import com.hyerim.book.springboot.web.dto.PostsUpdateRequestDto;
+import com.hyerim.book.springboot.web.dto.posts.PostsListResponseDto;
+import com.hyerim.book.springboot.web.dto.posts.PostsResponseDto;
+import com.hyerim.book.springboot.web.dto.posts.PostsSaveRequestDto;
+import com.hyerim.book.springboot.web.dto.posts.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -22,12 +22,12 @@ public class PostsService {
     private final PostsRepository postsRepository;
 
     @Transactional
-    public Long save(PostsSaveRequestDto requestDto) {
+    public Long savePost(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
     @Transactional
-    public Long update(Long id, PostsUpdateRequestDto requestsDto) {
+    public Long updatePost(Long id, PostsUpdateRequestDto requestsDto) {
         Posts posts = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
@@ -37,7 +37,7 @@ public class PostsService {
     }
 
     @Transactional(readOnly = true)
-    public PostsResponseDto findById(Long id) {
+    public PostsResponseDto findByIdInPost(Long id) {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
@@ -46,14 +46,14 @@ public class PostsService {
 
     //전체 글 내림차순 정렬
     @Transactional(readOnly = true)
-    public List<PostsListResponseDto> findAllDesc() {
+    public List<PostsListResponseDto> findAllDescPost() {
         return postsRepository.findAllDesc().stream()
                 .map(PostsListResponseDto::new)
                 .collect(Collectors.toList());
     }
 
     @Transactional
-    public void delete(Long id) {
+    public void deletePost(Long id) {
         Posts posts = postsRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
@@ -62,25 +62,25 @@ public class PostsService {
 
     //페이징 처리
     @Transactional
-    public Page<Posts> getBoardList(Pageable pageable) {
+    public Page<Posts> getPostList(Pageable pageable) {
         return postsRepository.findAll(pageable);
     }
 
     @Transactional
-    public Boolean getListCheck(Pageable pageable) {
-        Page<Posts> saved = getBoardList(pageable);
-        Boolean check = saved.hasNext();
+    public Boolean getPostListCheck(Pageable pageable) {
+        Page<Posts> saved = getPostList(pageable);
+        Boolean postCheck = saved.hasNext();
 
-        return check;
+        return postCheck;
     }
 
     //검색기능
     @Transactional
-    public Page<Posts> search(String keyword, Pageable pageable) {
+    public Page<Posts> searchPost(String postsKeyword, Pageable pageable) {
 
-        Page<Posts> boardList = postsRepository.findByTitleContaining(keyword, pageable);
+        Page<Posts> postList = postsRepository.findByTitleContaining(postsKeyword, pageable);
 
-        return boardList;
+        return postList;
     }
 
 }
